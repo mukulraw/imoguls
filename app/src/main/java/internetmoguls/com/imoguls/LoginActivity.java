3262a138953cce04807016fe7dba7507a852b628
mukul <mukulraw199517@gmail.com>
2016-10-04 18:12:21 +0530

    sfsaf

---
+++ app/src/main/java/internetmoguls/com/imoguls/LoginActivity.java
@@ -0,0 +1,391 @@
+package internetmoguls.com.imoguls;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.SmoothViewPager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import INTERFACES.Register;
+import POJO.RegisterBean;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class LoginActivity extends AppCompatActivity {
+    public static Context context;
+    static SmoothViewPager pager;
+    public static final int CONNECTION_TIMEOUT=10000;
+    public static final int READ_TIMEOUT=15000;
+    static TextSwitcher title;
+    SharedPreferences pref;
+    static SharedPreferences.Editor edit;
+    static String e = "";
+    static String p = "";
+    static Boolean keepLogged = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        context = this;
+
+        overridePendingTransition(0 , R.anim.fade);
+
+        pref = getSharedPreferences("myPref" , Context.MODE_PRIVATE);
+        edit = pref.edit();
+
+        pager = (SmoothViewPager) findViewById(R.id.pager);
+        title = (TextSwitcher) findViewById(R.id.title);
+
+
+
+
+
+
+
+        title.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView () {
+                TextView myText = new TextView(LoginActivity.this);
+                myText.setTextSize(34);
+                myText.setTextColor(Color.WHITE);
+                return myText;
+            }
+        });
+
+        title.setText("Sign In");
+
+
+        title.setInAnimation(getApplicationContext() , R.anim.fade_in);
+        //title.setOutAnimation(getApplicationContext() , R.anim.fade_out);
+
+
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), 2);
+
+
+
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0)
+                {
+                    title.setText("Sign In");
+                }
+                else
+                {
+                    title.setText("Sign Up");
+                }
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        pager.setAdapter(mSectionsPagerAdapter);
+
+
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+
+        final int noOfTabs;
+
+        SectionsPagerAdapter(FragmentManager fm, int noOfTabs) {
+            super(fm);
+            this.noOfTabs = noOfTabs;
+        }
+
+
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return noOfTabs;
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+
+
+                    return new FirstPage();
+
+                case 1:
+
+                    return new SecondPage();
+
+            }
+            return null;
+        }
+    }
+
+
+
+    public static class FirstPage extends Fragment {
+
+
+        EditText email , password;
+        Button log;
+        TextView loggedIn;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.sign_in_page1, container, false);
+
+            email = (EditText)v.findViewById(R.id.email_login);
+            password = (EditText)v.findViewById(R.id.password_login);
+            loggedIn = (TextView)v.findViewById(R.id.keep_logged_in);
+
+            loggedIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if (!keepLogged)
+                    {
+                        loggedIn.setBackgroundResource(R.drawable.logged_in_false);
+                        keepLogged = true;
+                    }
+                    else
+                    {
+                        loggedIn.setBackgroundColor(Color.TRANSPARENT);
+                        keepLogged = false;
+                    }
+
+
+                }
+            });
+
+
+            log = (Button)v.findViewById(R.id.signin);
+
+
+            log.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    e = email.getText().toString();
+
+                    p = password.getText().toString();
+
+
+                    //add login logic
+
+                    Intent i = new Intent(getContext() , MainActivity.class);
+                    startActivity(i);
+                    getActivity().finish();
+
+                    //logoEmail("email" , e , p);
+
+
+
+
+
+                }
+            });
+
+
+            TextView goToCreatePage = (TextView)v.findViewById(R.id.create_page);
+            goToCreatePage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    pager.setCurrentItem(2 , true);
+
+                    title.setText("Sign Up");
+                    // title.setInAnimation(getApplicationContext() , R.anim.fade_in);
+                    ///title.setOutAnimation(getApplicationContext() , R.anim.fade_out);
+
+                }
+            });
+
+            return v;
+
+        }
+    }
+
+    public static class SecondPage extends Fragment {
+
+
+        EditText username , emailId , mobileNo , passw , retpassword , addre;
+        Button create;
+
+
+
+
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.sign_in_page2, container, false);
+            //  title.setText("Sign Up");
+            TextView goToLoginPage = (TextView)v.findViewById(R.id.login_pager);
+
+            username = (EditText)v.findViewById(R.id.username);
+            emailId = (EditText)v.findViewById(R.id.emailId);
+            mobileNo = (EditText)v.findViewById(R.id.mobile);
+            passw = (EditText)v.findViewById(R.id.password);
+            retpassword = (EditText)v.findViewById(R.id.retpassword);
+            addre = (EditText)v.findViewById(R.id.address);
+            create = (Button)v.findViewById(R.id.signup);
+
+
+
+            create.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    String user = username.getText().toString();
+                    String emai = emailId.getText().toString();
+                    String pass = passw.getText().toString();
+                    String retpa = retpassword.getText().toString();
+                    String addr = addre.getText().toString();
+                    String mob = mobileNo.getText().toString();
+
+
+                    if (retpa.equals(pass))
+                    {
+
+                        //add register logic
+
+                        regisEmail(user , emai , mob , pass , addr);
+
+                    }
+                    else
+                    {
+                        Toast.makeText(getActivity() , "Your password didn't match" , Toast.LENGTH_SHORT).show();
+                    }
+
+                    //new register(emai , user , pass).execute();
+                    //regisEmail("email" , user , emai , pass);
+                    //new register1(emai , pass).execute();
+
+
+                }
+            });
+
+
+
+
+
+
+
+
+
+            goToLoginPage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    pager.setCurrentItem(0 , true);
+
+                    title.setText("Sign In");
+                    //  title.setInAnimation(getApplicationContext() , R.anim.fade_in);
+                    //  title.setOutAnimation(getApplicationContext() , R.anim.fade_out);
+                }
+            });
+
+            return v;
+        }
+
+
+
+        public void regisEmail(String name , String email , String mobile , String password , String address)
+        {
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://samplelogin.affixwebsolution.com/")
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            Register cr = retrofit.create(Register.class);
+
+            Call<RegisterBean> call = cr.register(name , email , mobile , password , address);
+
+            call.enqueue(new Callback<RegisterBean>() {
+                @Override
+                public void onResponse(Call<RegisterBean> call, Response<RegisterBean> response) {
+
+
+
+
+                    Log.d("asdasdasd" , response.body().getMessage());
+
+                    username.setText("");
+                    emailId.setText("");
+                    mobileNo.setText("");
+                    passw.setText("");
+                    retpassword.setText("");
+                    addre.setText("");
+
+
+
+                }
+
+                @Override
+                public void onFailure(Call<RegisterBean> call, Throwable t) {
+
+                }
+            });
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(0 , R.anim.fade);
+    }
+}
