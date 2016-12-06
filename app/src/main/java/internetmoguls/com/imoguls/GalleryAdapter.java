1b3f4e49a89aded210e460d800c67beff42dd10d
mukul <mukulraw199517@gmail.com>
2016-12-06 19:10:56 +0530

    asd

--- app/src/main/java/internetmoguls/com/imoguls/GalleryAdapter.java
+++ app/src/main/java/internetmoguls/com/imoguls/GalleryAdapter.java
@@ -1,9 +1,15 @@
 package internetmoguls.com.imoguls;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,11 +39,22 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.gallery_model , parent , false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Gallery item = list.get(position);
+        ImageLoader loader = ImageLoader.getInstance();
+        loader.displayImage(item.getGalleryImage() , holder.image);
+
+        holder.name.setText(item.getGalleryTitle());
+        holder.content.setText(item.getGalleryDescription());
+
+
 
     }
 
@@ -48,12 +65,19 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
 
     class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView name , content;
+        ImageView image;
 
 
 
 
         public ViewHolder(View itemView) {
             super(itemView);
+
+            name = (TextView)itemView.findViewById(R.id.gallery_name);
+            content = (TextView)itemView.findViewById(R.id.gallery_content);
+            image = (ImageView)itemView.findViewById(R.id.gallery_image);
+
         }
     }
 }
